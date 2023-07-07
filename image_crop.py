ebe360fb601bff9f886d532c16b8ca9c1c98b864
Philipp Roshchin <96698288+PHILIPP111007@users.noreply.github.com>
2023-07-08 01:09:10 +0300

    Update image_crop.py

--- image_crop.py
+++ image_crop.py
@@ -1,17 +1,19 @@
 import os
-from PIL import Image
+from PIL import Image, ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 def crop_save():
 
-	# Создание папки назначения, если она не существует
+	Создание папки назначения, если она не существует
 	if not os.path.exists(dest_folder):
 		os.makedirs(dest_folder)
-
 
 	# Перебор всех файлов в папке с исходными фотографиями
 	for filename in os.listdir(source_folder):
 		if filename.endswith(".jpg"):
+
 			# Получение полного пути к исходному файлу
 			source_path = os.path.join(source_folder, filename)
 
@@ -21,40 +23,40 @@ def crop_save():
 			if not os.path.exists(full_path):
 				os.makedirs(full_path)
 
-			# Открытие фотографии с помощью PIL
 			image = Image.open(source_path)
-
-			# Размеры исходной фотографии
-			width, height = image.size
-
-			# Размеры одной части (высота остается прежней)
-			part_width = width // X
-			part_height = height // Y
+			width = image.size[0] // W
+			height = image.size[1] // H
 
 			# Перебор и сохранение каждой части фотографии
-			for i in range(X):
-				for j in range(Y):
+			for i in range(H):
+				for j in range(W):
 
-					left = 0
-					top = j * part_height
-					right = part_width
-					bottom = (j + 1) * part_height
-					part_image = image.crop((left, top, right, bottom))
+					# Обрезаем изображение
+					part_image = image.crop(
+						(
+							j * width, # left
+							i * height, # top
+							j * width + width, # right
+							i * height + height # bottom
+						)
+					)
 
 					# Генерация нового имени для части фотографии
 					part_filename = f"{filename[:-4]}_{i + j + 1}.jpg"
+					# Путь для сохранения фотографии
 					save_path = os.path.join(full_path, part_filename)
 
-					# Сохранение части фотографии
+					# Сохранение
 					if not os.path.exists(save_path):
 						part_image.save(save_path)
+					
+					# part_image.show()
 
-			# Закрытие исходного изображения
 			image.close()
 
 
-# Части фотографий
-X, Y = 11, 11
+# Разбиение на части фотографии, можно настраивать
+W, H = 2, 2
 
 if __name__ == '__main__':
 	# Путь к папке с исходными фотографиями
