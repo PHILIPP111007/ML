481fe937e0c3c59e7d2352604077a0a21a10bf45
Philipp Roshchin <r.phil@yandex.ru>
2025-07-20 02:13:05 +0300

    update

--- phils_model/main.c
+++ phils_model/main.c
@@ -1,4 +1,4 @@
-// clang -shared -fopenmp -o main.so -fPIC -O3 main.c src/functions.c src/activations.c src/loss.c src/init.c src/json.c src/adam.c src/forward.c src/backward.c
+// clang -shared -fopenmp -o main.so -fPIC -O3 main.c src/functions.c src/activations.c src/loss.c src/init.c src/json.c src/adam.c src/forward.c src/backward.c src/get_time.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -12,28 +12,8 @@
 #include "src/adam.h"
 #include "src/forward.h"
 #include "src/backward.h"
+#include "src/get_time.h"
 
-
-char* get_time() {
-    // Выделяем статический буфер для хранения строки времени
-    static char time_str[30];
-
-    // Получаем текущее время
-    time_t raw_time;
-    struct tm *time_info;
-    time(&raw_time);
-    time_info = localtime(&raw_time);
-
-    // Получаем миллисекунды
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    long milliseconds = ts.tv_nsec / 1000000;
-
-    // Форматируем строку времени
-    strftime(time_str, sizeof(time_str), "%d-%m-%Y %H:%M:%S", time_info);
-
-    return time_str;
-}
 
 void fit(
     double *dataset_samples,
---
+++ phils_model/src/get_time.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "get_time.h"
+
+
+char* get_time() {
+    // Выделяем статический буфер для хранения строки времени
+    static char time_str[30];
+
+    // Получаем текущее время
+    time_t raw_time;
+    struct tm *time_info;
+    time(&raw_time);
+    time_info = localtime(&raw_time);
+
+    // Получаем миллисекунды
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    long milliseconds = ts.tv_nsec / 1000000;
+
+    // Форматируем строку времени
+    strftime(time_str, sizeof(time_str), "%d-%m-%Y %H:%M:%S", time_info);
+
+    return time_str;
+}
