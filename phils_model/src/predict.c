3dc86d69a7b6d8d868f0728f656bc3a338b2b145
Philipp Roshchin <r.phil@yandex.ru>
2025-08-04 14:48:45 +0300

    fix

--- phils_model/src/predict.c
+++ phils_model/src/predict.c
@@ -11,8 +11,16 @@ void *predict_thread(void *arg) {
 
         float ***Y = malloc(task->layer_sizes_rows * sizeof(float**));
 
+        float **sample = malloc(task->dataset_samples_cols * sizeof(float*));
+        for (int j = 0; j < task->dataset_samples_cols; j++) {
+            sample[j] = malloc(task->dataset_samples_depth * sizeof(float));
+            for (int k = 0; k < task->dataset_samples_depth; k++) {
+                sample[j][k] = task->sample[j][k];
+            }
+        }
+
         // Forward pass
-        forward(task->sample, task->dataset_samples_cols, task->dataset_samples_depth, task->weights, task->biases, Y, task->layer_sizes, task->layer_sizes_rows, task->layer_sizes_cols, task->activations);
+        forward(sample, task->dataset_samples_cols, task->dataset_samples_depth, task->weights, task->biases, Y, task->layer_sizes, task->layer_sizes_rows, task->layer_sizes_cols, task->activations);
 
         // Get predictions from last layer
         for (int j = 0; j < task->n_neurons_last_layer; j++) {
@@ -21,6 +29,7 @@ void *predict_thread(void *arg) {
         }
 
         // Free memory
+
         for (int layer_index = 0; layer_index < task->layer_sizes_rows; layer_index++) {
             for (int i = 0; i < task->dataset_samples_cols; i++) {
                 free(Y[layer_index][i]);
